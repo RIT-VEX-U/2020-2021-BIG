2a5cc6c412d209594908231ec054efee9823522f
Ryan McGee <ramtech51@comcast.net>
2020-05-17 15:47:54 -0400

    got rid of DUMB ERROR with WRONG PATHS

---
+++ include/core.h
@@ -0,0 +1,17 @@
+/**
+ * core.h
+ * Ryan McGee
+ *
+ * Include all files from the 'core' library
+ */
+
+//Subsystems
+#include "../core/include/subsystems/rgb_controller.h"
+#include "../core/include/subsystems/tank_drive.h"
+
+//Utils
+#include "../core/include/utils/pid.h"
+#include "../core/include/utils/spline_path.h"
+
+//Top Level
+#include "../core/include/pathfinder.h"
